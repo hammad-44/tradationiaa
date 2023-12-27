d52acccc6c3198933b571f31a7ce17be871804f5
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-12-27 23:39:00 +0500

    Added Some Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,  "static")
-
+# STATIC_ROOT = os.path.join(BASE_DIR,  "static")
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,  "static")
+    # other directories if necessary
+]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
